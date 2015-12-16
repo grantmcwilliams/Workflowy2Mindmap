de85bb8db852e723826b636ddf98535fc7193865
Grant McWilliams <grant@soundlinuxtraining.com>
2015-12-15 17:22:57 -0800

    Changed gdrive functionality to be optional.

--- workflowy2mm.sh
+++ workflowy2mm.sh
@@ -1,14 +1,44 @@
 #!/bin/bash
 
 CMDPATH=${0%/*}
+XSLFILE="${CMDPATH}/opml2mm.xsl"
+GDRIVE='false'
+
+syntax()
+{
+	echo "Usage:"
+	echo "${0##*/} <OPML file> [destination directory] [-g]"
+	echo ""
+	echo "Options:"
+	echo "	-g	upload mindmap file to Google Drive"
+	echo ""
+	echo "Destination directory is optional, if no provided"
+	echo "mindmap file will be in same directory as OPML file."
+	echo ""
+}
+
+while getopts :g OPTIONS
+do	
+	case "$OPTIONS" in
+		g)  GDRIVE='true';;
+		[?]) syntax ;;
+	esac
+done
+shift $((OPTIND-1))
+
 
 OPMLFILE="${1}"
+if [[ -z ${OPMLFILE} ]] ;then
+	syntax
+	exit 1
+fi
+
 DESTDIR="${2}"
 if [[ -z "${DESTDIR}" ]] ;then
 	DESTDIR="${PWD}"
 fi
 DESTFILE="${DESTDIR}/${OPMLFILE%.*}.mm"
-XSLFILE="${CMDPATH}/opml2mm.xsl"
+
 
 # Check to see if xsltproc exists in the path
 if ! hash xsltproc 2>/dev/null ;then
@@ -22,6 +52,12 @@ if ! hash drive 2>/dev/null ;then
 	exit 1
 fi
 
+# Convert OPML to Mindmap
 xsltproc -o "${DESTFILE}" "${XSLFILE}" "${OPMLFILE}"
-drive upload -f "${DESTFILE}"
+
+# If GDRIVE = true then attempt to upload file
+if [[ "${GDRIVE}" = 'true' ]]; then 
+	drive upload -f "${DESTFILE}"
+fi
+
 
